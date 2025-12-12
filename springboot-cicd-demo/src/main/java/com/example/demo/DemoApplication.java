b74c58703f1a3c7e84801a40e95428752b52937d
phuongvp <phuongpv@ics-p.vn>
2025-12-12 18:29:25 +0700

    update test build

--- springboot-cicd-demo/src/main/java/com/example/demo/DemoApplication.java
+++ springboot-cicd-demo/src/main/java/com/example/demo/DemoApplication.java
@@ -19,6 +19,6 @@ class HelloController {
 
     @GetMapping("/hello")
     public String hello() {
-        return "Hello CI/CD with GitHub Actions + Docker!";
+        return "Hello CI/CD with GitHub Actions + Docker updated!";
     }
 }
